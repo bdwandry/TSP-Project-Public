a514d2f0a8dde9c682983bc3ba094cc357702f49
teukkis <teemu.varsala@gmail.com>
2019-11-04 22:04:47 -0500

    comments added

--- backEnd/UDP/serial.js
+++ backEnd/UDP/serial.js
@@ -5,20 +5,28 @@ const Readline = require('@serialport/parser-readline');
 const port = new SerialPort('COM6', { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));
 
+
+//Open serialport and start listening incoming messages
 const arduinoOpen = () => {
     port.on("open", () => {
         console.log('Serial connection on');
         parser.on('data', (data) => {
+
+          //call lightChanges function in ../utils/arduinoHelper.js
           arduinoHelper.lightChanges(data)
         });
     });
 }
 
 
+//RoomNumber and state comes from ../controllers/lights.js
+//Send message to arduino which lights to be changed
 const arduinoSend = (roomNumber, state) => {
 
+  //call lightToSwitch function in ../utils/arduinoHelper.js
   let lightToSwitch =  arduinoHelper.translateRoom(roomNumber, state)
 
+  //send info to arduino which light will be turned on/off
   port.write(lightToSwitch, (err) => {
     if (err) {
       return console.log('Error on write: ', err.message);
